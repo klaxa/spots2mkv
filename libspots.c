e233b2692d0a36e4c774225e8702a24b5197d97c
Stephan Holljes <klaxa1337@googlemail.com>
2016-05-18 02:48:50 +0200

    Actually use libspots.h in libspots.c

--- libspots.c
+++ libspots.c
@@ -3,33 +3,8 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define SPOTS_HEADER_SIZE 52
-#define OGG_HEADER_SIZE 26
+#include "libspots.h"
 
-
-
-struct OggStream {
-    char magic[5];
-    int fresh;
-    int bos;
-    int eos;
-    int64_t ts;
-    int32_t serialno;
-    int32_t page_counter;
-    int64_t num;
-    int64_t denom;
-    int8_t shift;
-    int num_segments;
-    int size;
-    uint8_t *data;
-} OggStream;
-
-
-struct SpotsData {
-    char magic[4];
-    uint8_t res_x, res_y;
-    char *image_format;
-} SpotsData;
 
 void spots_dump_header(uint8_t *buf)
 {
